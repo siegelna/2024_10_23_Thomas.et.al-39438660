e8f7560b1ada3472b0a5076f524ae228cbe940b2
siegelna <nsiegel@anaptysbio.com>
2024-10-23 21:53:54 +0000

    test

--- R/02_immune_shiny_files.R
+++ R/02_immune_shiny_files.R
@@ -3,33 +3,33 @@
 ## bcell
 seu =  get(load(file = "data/bcell_viz_ready_revised.rda"))
 scConf1 = createConfig(seu)
-makeShinyFiles(seu, scConf1,gene.mapping = TRUE,shiny.prefix = "sc1",shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_immune/",gex.assay = "SCT", gex.slot = "scale.data")
+makeShinyFiles(seu, scConf1,gene.mapping = TRUE,shiny.prefix = "sc1",shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_immune/",gex.assay = "RNA")
 
 ## cd4
 seu =  get(load(file = "data/cd4tcell_viz_ready_revised.rda"))
 scConf2 = createConfig(seu)
-makeShinyFiles(seu, scConf2, gene.mapping = TRUE, shiny.prefix = "sc2", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_immune/", gex.assay = "SCT", gex.slot = "scale.data")
+makeShinyFiles(seu, scConf2, gene.mapping = TRUE, shiny.prefix = "sc2", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_immune/", gex.assay = "RNA")
 
 ## cd8
 seu =  get(load(file = "data/cd8tcell_viz_ready_revised.rda"))
 scConf3 = createConfig(seu)
-makeShinyFiles(seu, scConf3, gene.mapping = TRUE, shiny.prefix = "sc3", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_immune/", gex.assay = "SCT", gex.slot = "scale.data")
+makeShinyFiles(seu, scConf3, gene.mapping = TRUE, shiny.prefix = "sc3", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_immune/", gex.assay = "RNA")
 
 
 ## ilc
 seu =  get(load(file = "data/ilc_viz_ready_revised.rda"))
 scConf4 = createConfig(seu)
-makeShinyFiles(seu, scConf4, gene.mapping = TRUE, shiny.prefix = "sc4", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_immune/", gex.assay = "SCT", gex.slot = "scale.data")
+makeShinyFiles(seu, scConf4, gene.mapping = TRUE, shiny.prefix = "sc4", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_immune/", gex.assay = "RNA")
 
 ## myeloid
 seu =  get(load(file = "data/myeloid_viz_ready_revised.rda"))
 scConf5 = createConfig(seu)
-makeShinyFiles(seu, scConf5, gene.mapping = TRUE, shiny.prefix = "sc5", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_immune/", gex.assay = "SCT", gex.slot = "scale.data")
+makeShinyFiles(seu, scConf5, gene.mapping = TRUE, shiny.prefix = "sc5", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_immune/", gex.assay = "RNA")
 
 ## plasma
 seu =  get(load(file = "data/plasmacell_viz_ready_revised.rda"))
 scConf6 = createConfig(seu)
-makeShinyFiles(seu, scConf6, gene.mapping = TRUE, shiny.prefix = "sc6", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_immune/", gex.assay = "SCT", gex.slot = "scale.data")
+makeShinyFiles(seu, scConf6, gene.mapping = TRUE, shiny.prefix = "sc6", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_immune/", gex.assay = "RNA")
 
 
 ## Make files
--- R/02_structural_shiny_files.R
+++ R/02_structural_shiny_files.R
@@ -3,22 +3,22 @@
 ## endothelium
 seu =  get(load(file = "data/endothelium_glial_viz_ready_revised.rda"))
 scConf1 = createConfig(seu)
-makeShinyFiles(seu, scConf1,gene.mapping = TRUE,shiny.prefix = "sc1",shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_structural/",gex.assay = "SCT", gex.slot = "scale.data")
+makeShinyFiles(seu, scConf1,gene.mapping = TRUE,shiny.prefix = "sc1",shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_structural/",gex.assay = "RNA")
 
 ## epithelium_ileal
 seu =  get(load(file = "data/epi_ileal_viz_ready_revised.rda"))
 scConf2 = createConfig(seu)
-makeShinyFiles(seu, scConf2, gene.mapping = TRUE, shiny.prefix = "sc2", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_structural/", gex.assay = "SCT", gex.slot = "scale.data")
+makeShinyFiles(seu, scConf2, gene.mapping = TRUE, shiny.prefix = "sc2", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_structural/", gex.assay = "RNA")
 
 ## epithelium_colonic
 seu =  get(load(file = "data/epithelium_colonic_viz_ready_revised.rda"))
 scConf3 = createConfig(seu)
-makeShinyFiles(seu, scConf3, gene.mapping = TRUE, shiny.prefix = "sc3", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_structural/", gex.assay = "SCT", gex.slot = "scale.data")
+makeShinyFiles(seu, scConf3, gene.mapping = TRUE, shiny.prefix = "sc3", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_structural/", gex.assay = "RNA")
 
 ## fibroblast_pericyte
 seu =  get(load(file = "data/fib_peri_viz_ready_revised.rda"))
 scConf4 = createConfig(seu)
-makeShinyFiles(seu, scConf4, gene.mapping = TRUE, shiny.prefix = "sc4", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_structural/", gex.assay = "SCT", gex.slot = "scale.data")
+makeShinyFiles(seu, scConf4, gene.mapping = TRUE, shiny.prefix = "sc4", shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_structural/", gex.assay = "RNA")
 
 ## Make files
 makeShinyCodesMulti( shiny.title = "IDB Tissue Biopsies scRNAseq (13965420), Immune Cells", shiny.footnotes = NULL, shiny.prefix = c("sc1", "sc2", "sc3", "sc4"), shiny.headers = c("Endothelium", "Epithelium (Ileal)", "epithelium (Colonic)", "fibroblast and Pericyte"),  shiny.dir = "IBD_biopsy_scRNAseqzenodoID13965420_structural/") 
